b48f4d43b8a9f0de82a63c465994f58a9bc4ca7a
Deexith P <deexith@jmangroup.com>
2026-02-09 11:48:40 +0530

    feat : add file

---
+++ JMG198-Training/backend.java
@@ -0,0 +1,9 @@
+class Main {
+    public static void main(String[] args){
+        // Health Probe
+        System.out.println(" Server is Live ");
+
+        // Backend Endpoints ( GET )
+        System.out.println(" Backend Endpoint created -> /platform-engineering-app/health ");
+    }
+}
